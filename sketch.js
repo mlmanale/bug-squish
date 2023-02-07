5fd5a5aad83fd6b496f45f9b3dcf32102dccf05a
mlmanale <97977951+mlmanale@users.noreply.github.com>
2023-02-07 17:58:19 -0600

    multiple bugs

--- sketch.js
+++ sketch.js
@@ -3,20 +3,32 @@ let killCount = 0;
 let insideX;
 let insideY;
 let bugTimer = 30;
+let screenWidth = 400;
+let speed =  Math.floor(Math.random() * 10) + 1;
+let buggy = [];
+let totalBugs = 3;
 
 function setup() {
-  createCanvas(400, 400);
-  bug1 = new Bugs(20,20,50,50,true);
+  createCanvas(screenWidth, 400);
+  
+}
+
+function preload() {
+  for(let i=0; i < totalBugs; i++) {
+    buggy[i] = new Bugs();
+  }
 }
 
 function draw() {
   background(220);
   text('Time remaining: ' + bugTimer, 0, 10);
   text('Bugs killed: ' + killCount, 0,20);
-  bug1.update();
-
+  for(let i=0; i < totalBugs; i++) {
+    buggy[i].update();
+  }
 }
 
+//timer
 let timer = setInterval(()=>{
   if (bugTimer > 0) {
     bugTimer--;
@@ -26,43 +38,64 @@ let timer = setInterval(()=>{
   }
 }, 1000)
 
+//killcount and kill
 function mousePressed(){
-  let contains = bug1.contains(mouseX,mouseY);
-  if (contains == true){
-    killCount +=1;
+  for(let i=0; i < totalBugs; i++) {
+    let contains = buggy[i].contains(mouseX,mouseY);
+    if (contains == true){
+      killCount +=1;
+      for(let i=0; i < totalBugs; i++) {
+        if (buggy.xVelocity != 0) {
+          buggy[i].stop();
+        }
+      }
+    }
   }
 }
 
+//bug class
 class Bugs {
-  constructor(x,y,height,width,alive) {
-    this.height = height;
-    this.width = width;
+  constructor() {
+    this.height = 50;
+    this.width = 50;
     this.x = Math.floor(Math.random() * (400 - this.width));
     this.y = Math.floor(Math.random() * (400 - this.height));
-    this.xVelocity = 0;
-    this.alive = alive;
+    this.xVelocity =Math.random() < 0.5 ? (Math.floor(Math.random() * 10) + 1) : -(Math.floor(Math.random() * 10) + 1);
+    this.alive = true;
     //this.ss = ss;
-    //this.dir = dir;
-    //this.alive = alive; 
-
+    this.dir = Math.random() < 0.5 ? 1 : -1;
+    if (this.dir == 0) {
+      this.dir = -1;
+    }
+    
   }
     draw() {
-    // push();
-    //   scale(this.dir,1);
-    //   image(this.ss, this.dir*this.x,this.y,sh,sw,u*sh,v*sw,sh,sw);
-    // pop();
-    // }
+    push();
+      scale(this.dir,1);
+      rect(this.dir*this.x,this.y,this.height,this.width);
+    pop();
     fill('red');
-    rect(this.x,this.y,this.height,this.width);
     }
     
     update(){
       this.draw()
-      //this.x += this.xVelocity;
+      this.x += this.xVelocity;
+      if (this.x >= screenWidth) {
+        this.x = 1;
+      }
+      if (this.x <= 0) {
+        this.x = screenWidth - 1;
+      }
     }
+    
+    //checker for bug
     contains(x,y) {
       insideX = x >= this.x && x<= this.x + this.width;
       insideY = y >= this.y && y <= this.y + this.height;
       return insideX && insideY;
+    }
+    
+    stop() {
+      this.xVelocity = 0;
     }
     }
